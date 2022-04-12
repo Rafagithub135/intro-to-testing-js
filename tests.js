d205990991a8fe887407ccf437aff172776527e5
Ralph <mccloskey.ralph35@gmail.com>
2022-04-12 08:41:51 -0400

    completed tests

--- tests.js
+++ tests.js
@@ -18,6 +18,15 @@ describe('helloWorld', function() {
 
 describe('sayHello', function() {
     it('should be a defined function', function() {
-      expect(typeof sayHello).toBe('function');
+        expect(typeof sayHello).toBe('function');
+    });
+    it('should return a string when called', function() {
+        expect(typeof sayHello()).toBe("string");
+    })
+    it('should return the string "Hello" when eecuted', function () {
+        expect(sayHello()).toBe("Hello");
+    });
+    it('should never return "undefined" when called', function () {
+        expect(sayHello()).not.toBe(undefined);
     });
 });
